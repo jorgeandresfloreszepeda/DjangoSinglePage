27224f7026869f92f88c401e2e58c10023e931a9
rankipicard <jorge.andres.flores.zepeda@gmail.com>
2024-03-10 18:01:47 -0400

    Templates

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,4 +3,5 @@ from mainapp import views
 
 urlpatterns = [
     path("",views.home,name="home"),
+    path("mainapp/<name>",views.hello_there,name="hello_there"),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,5 @@
+import re
+from django.utils.timezone import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -5,3 +7,28 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Bonjour Django, seconde time!")
+
+def hello_there(request, name):
+    print(request.build_absolute_uri()) #optional
+    return render(
+        request,
+        'mainapp/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
+
+def hello_there_old(request, name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    match_object = re.match("[a-zA-Z]+", name)
+    
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Bonjour! Ca va? " + clean_name + "! La date es " + formatted_now
+    return HttpResponse(content)
